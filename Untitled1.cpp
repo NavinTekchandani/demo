9a44b4886093077438c8a07d4dec150dfb3cb3f7
NavinTekchandani <navinutekchandani@gmail.com>
2023-10-20 08:54:11 +0530

    successful

---
+++ Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i;
+	cout<<"-----Print numbers from 1 to 10------"<<endl;
+	for(i=1;i<=10;i++)
+	{
+		cout<<i<<endl;
+	}
+	return 0;
+}
